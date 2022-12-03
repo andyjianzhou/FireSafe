0f1e5dd6aaa09990bdadbaef6a6903dfb298a396
Tyler Beach <109818844+TylerBeach@users.noreply.github.com>
2022-12-02 20:34:35 -0700

    Change 1
    import app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title('Hackathon App Title Here')
+st.write('This is a frog bro what')
+
+button = st.button('Log In')
+
+if button:
+    st.write('Redirecting..')
+
+# Streamlit run app.py
