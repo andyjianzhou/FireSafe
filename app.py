7deeff26a0a854c4eb9bdec79c6d5e352c5311f9
PandaOnTheFly <74941296+andyjianzhou@users.noreply.github.com>
2022-12-03 17:29:32 -0700

    Created and added API requests
    Used REST API's to gain information and display images and checkboxes

--- app.py
+++ app.py
@@ -26,29 +26,39 @@ st.title('Hackathon App Title Here')
 
 API_KEY = 'd40f2850435a4ea88e5c1d3736182c61'
 def autocomplete(parameters, API_KEY):
-    get_URL = f"https://api.geoapify.com/v1/geocode/search?name={parameters}apiKey={API_KEY}"
+    get_URL = f"https://api.geoapify.com/v1/geocode/search?text={parameters}&format=json&apiKey={API_KEY}"
     response = requests.get(get_URL)
     data = response.json()
     pprint.pprint(data)
+    return data
 
-autocomplete('Gaskell Rd, Rosamond', API_KEY)
+# convert location to array
+def location_to_array(location):
+    location = location.replace(',', "%2C%20")
+    location = location.replace(' ', "%20")
+    print(location)
+    return location
+
 col1, col3, col2 = st.columns(3, gap='large')
-
-
 total_images = []
 def load_image(image_path):
     image = Image.open(image_path)
     width, height = image.size
     return image, width, height
 
+found = False
 with col1:
     st.write('')
 
     # input react js code here
     location = st.text_input('Enter Location')
-    if location.casefold() == 'Gaskell Rd, Rosamond'.casefold():
+    if location:
+        location = location_to_array(location)
+        location = autocomplete(location, API_KEY)
         st.write("Location found!")
-        st.write("Location is" + "21505 Gaskell Rd, Rosamond, CA 93560, United States")
+        address = location['results'][0]['address_line2']
+        found = True
+
     if location:
         st.write("Input image for location")
         uploaded_file = st.file_uploader("Choose a file", ['png', 'jpg'], True, label_visibility='collapsed')
@@ -56,10 +66,10 @@ with col1:
         # display image
         if uploaded_file is not None:
             #image path
-            for uploaded_file in uploaded_file:
+            for i, uploaded_file in enumerate(uploaded_file):
                 image, width, height = load_image(uploaded_file)
                 total_images.append(image)
-                st.image(image, caption='Uploaded Image.', use_column_width=True)
+                st.image(image, caption= i+1, use_column_width=True)
                 st.write("")
                 st.write("Classifying...")
                 label = random.choice(LABELS)
@@ -67,9 +77,11 @@ with col1:
                 st.write("")
 
 with col2:
-    st.write('')
-
-    col2.subheader('Locations')
+    if found:
+        col2.header(address)
+        col2.subheader(f"Latitude: {str(location['results'][0]['lat'])} Longitude: {str(location['results'][0]['lon'])}")
+    else:
+        col2.header("No location found")
     if total_images:
         for i in range(len(total_images)):
-            st.checkbox(str(i))
+            st.checkbox(str(i+1))
